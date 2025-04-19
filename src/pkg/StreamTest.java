13cffaf6e2522e9049298c57be170f3e92218655
Ritwik Saha <riksaha1998@gmail.com>
2025-04-19 10:55:37 +0530

    Change in Stream API class

--- src/pkg/StreamTest.java
+++ src/pkg/StreamTest.java
@@ -21,7 +21,13 @@ public class StreamTest {
 		List<Integer> limitAndSkip = newList.stream().limit(4).skip(1).collect(Collectors.toList());
 		System.out.println(limitAndSkip);
 		
-		List<Integer> iterate = Stream.iterate(0, n->n+1).limit(21).skip(1).filter(x->x%2==0).map(x->x+1).sorted((a,b)-> (b-a)).peek(x->System.out.println(x)).collect(Collectors.toList());
+		/*
+		 * Stream.iterate things are shown below
+		 * Stream.iterate(0, n->n+1).limit(21).skip(1).filter(x->x%2==0).map(x->x+1).sorted((a,b)-> (b-a)).peek(x->System.out.println(x)).collect(Collectors.toList());
+		 */
+		
+		//Printing 0 to 21 and skipping the 1st value that is 0.
+		List<Integer> iterate = Stream.iterate(0, n->n+1).skip(1).limit(21).collect(Collectors.toList());
 		System.out.println(iterate);
 		
 		int i = list.stream().peek(x->System.out.println(x)).max((a,b) ->(a-b)).get();
@@ -34,7 +40,11 @@ public class StreamTest {
 		int[] mergeArray = IntStream.concat(Arrays.stream(arr), Arrays.stream(arr2)).toArray();
 		System.out.println(Arrays.toString(mergeArray));
 		
-		//Finding 
+		//Finding ODD and EVEN using Stream
+		List<Integer> numberList = Arrays.asList(1,2,3,5,6,7);
+		
+		List<Integer> odd = numberList.stream().filter(x->x%2!=0).collect(Collectors.toList());
+		System.out.println(odd);
 		
 
 	}
