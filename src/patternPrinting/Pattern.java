38939c787e07aef829c8a3ab5a29365fef2056f0
Ritwik Saha <riksaha1998@gmail.com>
2025-04-30 22:29:39 +0530

    Update Pattern.java

--- src/patternPrinting/Pattern.java
+++ src/patternPrinting/Pattern.java
@@ -113,6 +113,11 @@ public class Pattern {
 		}
 	}
 	public static void solidRectangle(int n) {
+// i	
+//	1	* * * * row and column are same so loop will run n times
+//	2	* * * * 
+//	3	* * * * 
+//	4	* * * * 
 		for(int i=0; i<n; i++) {
 			for(int j = 0; j<n; j++) {
 				System.out.print("* ");
