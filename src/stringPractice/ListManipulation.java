cf83cb744517868fc39473f4c0c75825d0bc3443
Ritwik Saha <riksaha1998@gmail.com>
2025-05-03 00:45:09 +0530

    Update ListManipulation.java

--- src/stringPractice/ListManipulation.java
+++ src/stringPractice/ListManipulation.java
@@ -9,11 +9,58 @@ public class ListManipulation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Reverse a list
 		List<Integer> originalList = Arrays.asList(1,2,3,4,5,6);
 		List<Integer> reversedList = new ArrayList<>(originalList);
         Collections.reverse(reversedList);
         
         System.out.println(reversedList);
+        
+        List<String> listOfString = Arrays.asList("Rit", "Anki", "Sup");
+        Collections.reverse(listOfString);       
+        System.out.println(listOfString);
+        
+        
+        //Check for missing number
+        int[] arr= {0,1,2,4,5,6,7,9};
+        System.out.println("checkForMissingNumber");
+        checkForMissingNumber(arr);
+        
+        
 	}
-
+	
+//			Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
+//			Output: 6
+//			Explanation: All the numbers from 1 to 8 are present except 6.
+//
+//
+//			Input: arr[] = [1, 2, 3, 5]
+//			Output: 4
+//			Explanation: Here the size of the array is 4, so the range will be [1, 5]. The missing number between 1 to 5 is 4
+	public static void checkForMissingNumber(int[] arr){
+		int length = arr.length+1;
+		System.out.println("Length = " + arr.length);
+		
+		int missingValue = 0;
+		for(int i = 1; i<=length; i++) {
+			boolean flag = false;
+			
+			for(int j= 0; j<arr.length; j++ ) 
+			{
+				if(arr[j] == i) 
+				{
+					flag=true;
+					break;
+				}
+				
+			}
+			if(!flag) {
+				System.out.println(i);
+			}
+		}		
+		
+    }
 }
+
+
